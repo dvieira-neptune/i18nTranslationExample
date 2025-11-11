fdf7ff35d87f3c550c18052253f6a72e754c25d1
Daniel Vieira <daniel.vieira@neptune-software.com>
2025-11-11 12:20:20 +0000

    now with extra goodies! :)

---
+++ artifacts/Script/Project/example_script_i18n/send_i18n_translated_email/content.js
@@ -0,0 +1,15 @@
+await sendEmail({
+    to: "lloyd.trevarthen@neptune-software.com",
+    from: "support@neptune-software.com",
+    templateId: "4DB3F70D-F7BE-F011-8194-0022489D3772", //change this ID
+    language: "EN",
+});
+
+await sendEmail({
+    to: "lloyd.trevarthen@neptune-software.com",
+    from: "support@neptune-software.com",
+    templateId: "4DB3F70D-F7BE-F011-8194-0022489D3772", //change this ID
+    language: "PT",
+});
+
+complete();
